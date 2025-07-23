99db5065b5e2a23c063a5613aae3b848371a88bc
kalebxsz <kalebgabriel720@gmail.com>
2025-07-22 23:20:42 -0300

    aprendendo if-else

---
+++ condicionais/index.js
@@ -0,0 +1,76 @@
+ /* let idade = 18;
+
+if (idade >=18){
+    console.log("Cliente pode comprar bebida");
+}else{
+    console.log("Cliente menor de 18 anos. Venda proibida");
+}
+
+
+let idade2 = 17;
+
+if (idade2 => 17){
+    console.log("Cliente menor de 18 anos. Venda proibida");
+}else{
+    console.log("cliente pode comprar bebida");
+} */
+
+
+/* let hora = 5;
+
+if(hora >=5 && hora <= 11){
+    console.log(`Bom dia meu querido`)
+}else if(hora > 11 && hora <= 17){
+    console.log(`Boa tarde meu querido`)
+}else{
+    console.log(`Boa noite meu querido`)
+}
+ */
+
+// operador Ternário 
+// (condição) ? se condição true : se condição false; 
+// Exemplo abaixo
+/*let idadeAluno = 14;
+
+let responsabilidade = (idadeAluno >= 18) ? "o aluno poderá assinar seu próprio boletim" : "O aluno, deverá comparecer com os seus pais, para a retirada do boletim"
+console.log(responsabilidade) */
+
+
+/* domingo - dia 00
+segunda - dia 01 
+terça - dia 02
+quarta - dia 03
+quinta - dia 04
+Sexta - dia 05
+sabado - dia 06 */
+
+let dia = 6;
+
+switch (dia) {
+    case 0:
+        console.log("domingo")
+        break;
+    case 1:
+        console.log("segunda")
+        break;
+    case 2:
+        console.log("terça")
+        break;
+    case 3:
+        console.log("quarta")
+        break;
+    case 4:
+        console.log("quinta")
+        break;
+    case 5:
+        console.log("Sexta")
+        break;
+    case 6:
+        console.log("Sabado")
+        break;
+    
+
+    default:
+        console.log("não é um dia válido")
+        break;
+}
